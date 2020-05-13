f20bea755469140b2ff4cbf377e2facd3273b495
[Xaxim] Carlos Lima <carloslima@mpdft.mp.br>
2020-05-13 17:30:47 -0300

    add code extensions

---
+++ windows/install-extensions-insiders.bat
@@ -0,0 +1,9 @@
+  
+code-insiders --install-extension aeschli.vscode-css-formatter
+code-insiders --install-extension Angular.ng-template
+code-insiders --install-extension berublan.vscode-log-viewer
+code-insiders --install-extension CoenraadS.bracket-pair-colorizer-2
+code-insiders --install-extension pflannery.vscode-versionlens
+code-insiders --install-extension PKief.material-icon-theme
+code-insiders --install-extension VisualStudioExptTeam.vscodeintellicode
+code-insiders --install-extension WakaTime.vscode-wakatime
---
+++ windows/install-extensions.bat
@@ -0,0 +1,9 @@
+  
+code --install-extension aeschli.vscode-css-formatter
+code --install-extension Angular.ng-template
+code --install-extension berublan.vscode-log-viewer
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension pflannery.vscode-versionlens
+code --install-extension PKief.material-icon-theme
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension WakaTime.vscode-wakatime
